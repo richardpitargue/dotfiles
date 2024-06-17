cae18b213ff65f617d81f53985dafd3777371f92
Richard Pitargue <8220621+richardpitargue@users.noreply.github.com>
2024-06-17 16:59:24 +0800

    add tmux vim navigator

--- .config/nvim/lua/richardpitargue/plugins.lua
+++ .config/nvim/lua/richardpitargue/plugins.lua
@@ -1,4 +1,21 @@
 require("lazy").setup({
   {"catppuccin/nvim", name = "catppuccin", priority = 1000},
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+  "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 })
